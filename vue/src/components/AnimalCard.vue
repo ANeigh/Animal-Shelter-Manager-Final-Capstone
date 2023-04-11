f42c858169bb5ddff4a98ae854011d08e283c53b
Austin Neigh <austinneigh@gmail.com>
2023-04-11 15:33:08 -0400

    added individual animal component and list of animals components

---
+++ vue/src/components/AnimalCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card">
+      <h2 class>{{animal.name}}</h2>
+      <img>
+      <h3>{{animal.breed}}</h3>
+      <h3>{{animal.gender}}</h3>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'animal-card',
+    props: {
+        book: Object
+    }
+
+}
+</script>
+
+<style>
+.card {
+    border: 2px solid black;
+    border-radius: 10px;
+    width: 250px;
+    height: 550px;
+    margin: 20px;
+}
+
+</style>
---
+++ vue/src/components/AnimalList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="book-container">
+      <animal-card
+      v-bind:animal="animals"
+      v-for="animal in $store.state.animals"
+      v-bind:key="animal.id"      
+      >
+  </div>
+</template>
+
+<script>
+
+import AnimalCard from './AnimalCard.vue';
+
+export default {
+    components: {
+        AnimalCard
+    }
+}
+</script>
+
+<style>
+.book-container {
+    display: flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+}
+
+</style>

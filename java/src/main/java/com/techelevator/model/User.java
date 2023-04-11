2370e65a3e4cabff9e8481fa5e86e0a3f8cb0299
Nicholas Natale <natalen005@gmail.com>
2023-04-11 15:27:45 -0400

    changing user model

--- java/src/main/java/com/techelevator/model/User.java
+++ java/src/main/java/com/techelevator/model/User.java
@@ -15,19 +15,17 @@ public class User {
    @JsonIgnore
    private boolean activated;
    private Set<Authority> authorities = new HashSet<>();
-   private String email;
-   private String phoneNumber;
+
+
 
    public User() { }
 
-   public User(int id, String username, String password, String authorities, String email, String phoneNumber) {
+   public User(int id, String username, String password, String authorities) {
       this.id = id;
       this.username = username;
       this.password = password;
       if(authorities != null) this.setAuthorities(authorities);
       this.activated = true;
-      this.email = email;
-      this.phoneNumber = phoneNumber;
    }
 
    public int getId() {
@@ -78,21 +76,7 @@ public class User {
       }
    }
 
-   public String getEmail() {
-      return email;
-   }
-
-   public void setEmail(String email) {
-      this.email = email;
-   }
-
-   public String getPhoneNumber() {
-      return phoneNumber;
-   }
-
-   public void setPhoneNumber(String phoneNumber) {
-      this.phoneNumber = phoneNumber;
-   }
+   
 
    @Override
    public boolean equals(Object o) {

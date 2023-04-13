d3b6f08aa5ed9c2f7f8570938adc22fae5470b55
Nicholas Natale <natalen005@gmail.com>
2023-04-13 17:08:42 -0400

    changes to the components ive been working on, combined welcome banner and nav bar

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link v-bind:to="{ name: 'login' }" v-if="!isLoggedIn">Login</router-link>
+      <router-link v-bind:to="{ name: 'logout' }" v-if="isLoggedIn">Logout</router-link>
     </div>
     <router-view />
   </div>
 </template>
+<script>
+export default {
+  computed: {
+    isLoggedIn() {
+      return !!this.$store.state.token;
+    }
+  }
+}
+</script>
+
+
 
 <style>
 #app{

b1397811807e71333ad31ffaaa3483fa9b304fef
Laura Almendinger <l.almendinger@gmail.com>
2023-04-14 08:40:32 -0400

    Created Volunteer view, ApplicationForm component. Prelim architecture

---
+++ vue/src/views/Volunteer.vue
@@ -0,0 +1,26 @@
+<template>
+  <section id="volunteer">
+    <h2>
+      <!-- Headline -->
+    </h2>
+    <p>
+      <!-- Body text -->
+    </p>
+    <ApplicationForm />
+  </section>
+</template>
+
+<script>
+import ApplicationForm from "../components/ApplicationForm.vue";
+
+export default {
+  name: "volunteer",
+  components: {
+    ApplicationForm,
+  },
+  
+};
+</script>
+
+<style>
+</style>

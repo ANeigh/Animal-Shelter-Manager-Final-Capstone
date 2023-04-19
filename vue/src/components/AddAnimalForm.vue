6b02e737a3ae466c7f2ce4647bc65431d65140eb
Austin Neigh <austinneigh@gmail.com>
2023-04-19 14:18:51 -0400

    added v-if conditions on update and add animal form buttons

--- vue/src/components/AddAnimalForm.vue
+++ vue/src/components/AddAnimalForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <section>
+  <section v-if="isLoggedIn">
     <div class="container">
       <div class="header">
         <button class="button is-link is-outlined" @click="toggleForm">
@@ -162,6 +162,11 @@ export default {
         url: "",
       },
     };
+  },
+  computed: {
+    isLoggedIn () {
+      return !!this.$store.state.token;
+    }
   },
   methods: {
     openUploadModal() {

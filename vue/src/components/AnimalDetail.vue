a2071403d5d564f54fa9fd4e4c746afb0a9afc65
Jared Hance <jarhance@gmail.com>
2023-04-19 14:32:05 -0400

    animal detail page

--- vue/src/components/AnimalDetail.vue
+++ vue/src/components/AnimalDetail.vue
@@ -1,44 +1,70 @@
 <template>
-  <section>
-      <figure v-for="(imgs, index) in getAnimalImages(animal.animalId)" v-bind:key="index">
-          <img class="images" :src="imgs.url">
-      </figure>
-      <p>{{animal.name}}</p>
-      <p>{{animal.breed}}</p>
-      <p>{{animal.gender}}</p>
-      <p>{{animal.age}}</p>
-      <p>{{animal.color}}</p>
-      <p>{{animal.tags}}</p>
-      <p>{{animal.description}}</p>
-      
+  <section class="section">
+    <carousel class="images">
+      <slide v-for="(imgs, index) in getAnimalImages(animal.animalId)"
+      v-bind:key="index"> <img :src="imgs.url"> </slide>
+    </carousel>
+    <div class="information">
+    <p class="name">Name: {{ animal.name }}</p>
+    <p class = "name">Breed: {{ animal.breed }}</p>
+    <p class = "name">Gender: {{ animal.gender }}</p>
+    <p class = "name">Age: {{ animal.age }}</p>
+    <p class = "name">Color: {{ animal.color }}</p>
+    <p class = "name" v-show="animal.tags">Tags: {{ animal.tags }}</p>
+    <p class = "description">{{ animal.description }}</p>
+    </div>
   </section>
 </template>
 
 <script>
+import { Carousel, Slide } from "vue-carousel";
 
 export default {
-    name: 'animaldetail',
-    props: ['animal'],
-    methods: {
-        getAnimalImages(animalId) {
-            const animalImages = this.$store.state.imgs.filter(img => img.animalId === animalId);
-            return animalImages;
-        }
+  name: "animaldetail",
+  props: ["animal"],
+  components: {
+    Carousel,
+    Slide,
+  },
+  methods: {
+    getAnimalImages(animalId) {
+      const animalImages = this.$store.state.imgs.filter(
+        (img) => img.animalId === animalId
+      );
+      return animalImages;
     },
-}
+  },
+};
 </script>
 
 
 <style>
-.images{
-    width: auto;
-    height: auto;
-    max-width: 200px;
-    max-height: 200px;
-    margin-top: 200px;
+.images {
+  width: auto;
+  height: auto;
+  max-width: 600px;
+  max-height: 600px;
+  align-content: center;
+  justify-content: center;
+  margin: 0 auto;
+}
+.information {
+    margin: 0 auto;
+    text-align: center;
+}
+.name {
+    align-content: center;
+    justify-content: center;
+    margin: 0 auto;
+    background-color: #7C909E
+}
+.description {
+    background-color:#a28f9d;
+}
+.section {
+    background-color: #235789;
+
 }
 
-.animal-details{
-    
-}
+
 </style>
